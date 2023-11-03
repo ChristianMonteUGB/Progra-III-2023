f087bc82c1838c6814923eae824dabd33bdec214
Christian montenegro <109983821+ChristianMonteUGB@users.noreply.github.com>
2023-11-03 16:05:16 +0000

    cambios 3/11 v2

--- Appacademica/urls.py
+++ Appacademica/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from appalumnos.views import saludo, miEdad, index
+from appalumnos.views import saludo, miEdad, index, alumnos, busqueda_alumnos, materias, busqueda_materias
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('edad/<int:edad>/', miEdad),
      path('', index),
+    path('frmalumnos', alumnos),
+    path('frmmaterias', materias),
+    path('frmbusqueda_alumnos', busqueda_alumnos),
+    path('frmbusqueda_materias', busqueda_materias),
 ]
